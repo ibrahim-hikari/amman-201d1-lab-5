ebb08f7a16631caba87290d0c384c933c86e0ac9
ibrahim-hikari <55652442+ibrahim-hikari@users.noreply.github.com>
2019-10-12 17:45:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -83,45 +83,19 @@ Test this function by hand in the console to get it working, and when you think 
 //eslint-disable-line
 
 function sumArray(testArray) { //eslint-disable-line
-    var testArray = [2, 3, 4];
-    var firstSum = sum(testArray[0], testArray[1]);
-    var secondSum = sum(firstSum[0], testArray[2]);
-    return [secondSum[0], "2,3,4 was passed in as an array of numbers, and 9 is their sum."]
-}
-
-    // Here is the test for sumArray(); uncomment it to run it
-
-    // testSumArray(testArray);
-
-function sumArray(testArray) { //eslint-disable-line
     testArray = [2, 3, 4];
     var resultSum = sum(sum(testArray[0], testArray[1])[0], testArray[2]);
     return [resultSum[0], testArray[0] + ' ' + testArray[1] + ' ' + testArray[2] + ' was passed in as an array of numbers, and ' + resultSum[0] + ' is their sum.']
 }
 
+
+    // Here is the test for sumArray(); uncomment it to run it
+
+    // testSumArray(testArray);
+
+
     // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
-    /////////////////////////////////////
-    /* Problem 5
-    Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
-    
-    "The numbers 2,3,4 have a product of 24."
-    
-    IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. This function should handle an array containing three elements. However, you may continue to use the + operator for string concatenation.
-    
-    Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
-
-    // Write your code here
-    function multiplyArray(multArr) { //eslint-disable-line
-
-    }
-
-    // Here is the test for multiplyArray(); uncomment it to run it
-    // testMultiplyArray(testArray);
-
-    // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
-
-    // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
 /////////////////////////////////////
 /* Problem 5
 Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
